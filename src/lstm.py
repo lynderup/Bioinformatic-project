156afe64976c93bb056ecf583857fbebc134a589
Lynderup <lynderupjensen@gmail.com>
2017-04-17 16:35:58 +0200

    rnn code now in different file

---
+++ src/lstm.py
@@ -0,0 +1,149 @@
+import tensorflow as tf
+import numpy as np
+
+def sequence_cross_entropy(labels, logits, sequence_lengths):
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
+    if sequence_lengths is not None:
+        loss_sum = tf.reduce_sum(cross_entropy, axis=0)
+        return tf.truediv(loss_sum, tf.cast(sequence_lengths, tf.float32))
+    else:
+        return tf.reduce_mean(cross_entropy, axis=0)
+
+
+class Model:
+
+    def __init__(self, config,  inputs, targets, batch_size, sequence_lengths=None, is_training=False):
+        global_step = tf.Variable(0, trainable=False)
+
+        # cell = tf.contrib.rnn.BasicLSTMCell(num_units=size, forget_bias=0.0, state_is_tuple=True)
+        # cell = tf.contrib.rnn.LSTMBlockCell(num_units=size, forget_bias=1.0)
+        #
+        # with tf.name_scope("initial_state"):
+        #     initial_state = cell.zero_state(batch_size=batch_size, dtype=tf.float32)
+        #     # tf.summary.tensor_summary('initial_state', initial_state)
+
+        embedding = tf.get_variable("embedding", [config.num_classes, config.num_units], dtype=tf.float32)
+        _inputs = tf.nn.embedding_lookup(embedding, inputs)
+
+        # outputs, state = tf.contrib.rnn.static_rnn(cell, _inputs, initial_state=initial_state, scope="rnn", sequence_length=sequence_lengths)
+
+        lstm = tf.contrib.rnn.LSTMBlockFusedCell(num_units=config.num_units, forget_bias=0, cell_clip=None, use_peephole=False)
+
+        initial_state = (tf.zeros([batch_size, config.num_units], tf.float32), tf.zeros([batch_size, config.num_units], tf.float32))
+
+        output, state = lstm(_inputs, initial_state=initial_state, dtype=None, sequence_length=sequence_lengths, scope="rnn")
+
+        softmax_w = tf.get_variable("softmax_w", [config.num_units, config.num_classes], dtype=tf.float32)
+        softmax_b = tf.get_variable("softmax_b", [config.num_classes], dtype=tf.float32)
+        # logits = [tf.matmul(output, softmax_w) + softmax_b for output in outputs]
+
+        _output = tf.reshape(output, [-1, config.num_units])
+        _logits = tf.matmul(_output, softmax_w) + softmax_b
+
+        logits = tf.reshape(_logits, [-1, batch_size, config.num_classes])
+
+        loss = tf.reduce_mean(sequence_cross_entropy(labels=targets, logits=logits, sequence_lengths=sequence_lengths))
+
+        self.logits = logits
+        self.loss = loss
+
+        if not is_training: return
+
+        starting_learning_rate = 0.1
+        learning_rate = tf.train.exponential_decay(starting_learning_rate, global_step, 10, 0.96, staircase=True)
+
+        optimizer = tf.train.AdamOptimizer(learning_rate)
+
+        trainable_vars = tf.trainable_variables()
+
+        train_step = optimizer.minimize(loss, var_list=trainable_vars, global_step=global_step)
+
+        self.learning_rate = learning_rate
+        self.train_step = train_step
+
+
+def train(config, summary_writer):
+    batch_size = config.batch_size
+
+    with tf.name_scope("Train"):
+
+        inputs = tf.placeholder(tf.int32, shape=(None, batch_size), name="inputs")
+        targets = tf.placeholder(tf.int32, shape=(None, batch_size), name="targets")
+        sequence_lengths = tf.placeholder(tf.int32, shape=(batch_size,), name="sequence_lengths")
+
+        with tf.variable_scope("Model", reuse=None):
+            train_model = Model(config, inputs, targets, batch_size, sequence_lengths=sequence_lengths, is_training=True)
+
+        tf.summary.scalar("loss", train_model.loss)
+        tf.summary.scalar("learning rate", train_model.learning_rate)
+
+
+
+    merged = tf.summary.merge_all()
+    init_op = tf.global_variables_initializer()
+
+    with tf.Session() as session:
+
+        session.run(init_op)
+
+        train_dataset = config.train_dataset
+
+        for i in range(config.epochs):
+
+            train_dataset.shuffle()
+            batches = train_dataset.partition(batch_size)
+
+            for _inputs, _targets, _sequence_lengths in batches:
+
+                feed_dict = {inputs: _inputs,
+                             targets: _targets,
+                             sequence_lengths: _sequence_lengths}
+
+                if i % 10 == 0:
+                    print(i)
+                    summary = session.run(merged, feed_dict=feed_dict)
+                    summary_writer.add_summary(summary, i)
+
+                session.run(train_model.train_step, feed_dict=feed_dict)
+
+
+def test(config, summary_writer):
+    batch_size = 1
+
+    with tf.name_scope("Test"):
+        inputs = tf.placeholder(tf.int32, shape=(None, batch_size), name="inputs")
+        targets = tf.placeholder(tf.int32, shape=(None, batch_size), name="targets")
+        sequence_lengths = tf.placeholder(tf.int32, shape=(batch_size,), name="sequence_lengths")
+
+        with tf.variable_scope("Model", reuse=True):
+            test_model = Model(config, inputs, targets, batch_size, sequence_lengths=sequence_lengths)
+
+        tf.summary.scalar("loss", test_model.loss)
+
+    # merged = tf.summary.merge_all()
+    init_op = tf.global_variables_initializer()
+
+    with tf.Session() as session:
+        session.run(init_op)
+
+        test_dataset = config.test_dataset
+        test_dataset.shuffle()
+        batches = test_dataset.partition(batch_size)
+
+        for _inputs, _targets, _sequence_lengths in batches:
+            feed_dict = {inputs: _inputs,
+                         targets: _targets,
+                         sequence_lengths: _sequence_lengths}
+
+            logits = test_model.logits
+            loss = test_model.loss
+
+            # summary, out, targ, loss = session.run([merged, logits, test_targets, loss])
+            out, loss = session.run([logits, loss], feed_dict=feed_dict)
+
+            # np.set_printoptions(precision=5)
+            pretty_out= np.array([[np.argmax(batch) for batch in _batches] for _batches in out])
+            pretty_targets = np.array([i.tolist() for i in _targets])
+
+            print(loss)
+            print(np.swapaxes(np.array([pretty_out, pretty_targets]), 0, 2))
--- src/main.py
+++ src/main.py
@@ -2,172 +2,52 @@
 import tensorflow as tf
 import itertools as it
 import numpy as np
+import lstm
 
 import reader
 
-class ModelConfig(object):
+
+num_seq = [1, 2, 4, 5, 6, 7, 8, 9, 10]
+test_step_size = 2
+
+
+def get_dummy_datasets(num_classes):
+
+    _inputs = [[j for j in range(0, num_classes, i)] for i in num_seq]
+
+
+    dummy_train_dataset = reader.Dataset(raw_data=np.array(_inputs), raw_labels=np.array(_inputs))
+
+    test_input = np.array([[i for i in range(0, num_classes, test_step_size)]])
+    test_targets = np.array([[i for i in range(0, num_classes, test_step_size)]])
+
+    dummy_test_dataset = reader.Dataset(raw_data=test_input, raw_labels=test_targets)
+
+    return dummy_train_dataset, dummy_test_dataset
+
+
+class DummyModelConfig(object):
     num_units = 10
     num_classes = 100
 
+    batch_size = len(num_seq)
+
     epochs = 200
 
-    num_seq = [1, 2, 4, 5, 6, 7, 8, 9, 10]
-    test_step_size = 3
-
-# test1 = [i for i in range(0, num_classes, 1)]
-# test2 = [i for i in range(0, num_classes, 2)]
-#
-#
-# inputs = [test1, test2]
-
-def get_input(config):
-    inputs = [[j for j in range(0, config.num_classes, i)] for i in config.num_seq]
-
-    # tf_inputs = [tf.convert_to_tensor([*[i for i in input]], dtype=tf.int32) for input in it.zip_longest(*inputs, fillvalue=0)]
-    # targets = [tf.convert_to_tensor(inputs, dtype=tf.int32) for inputs in it.zip_longest(*inputs, fillvalue=0)]
-
-    tf_inputs = tf.convert_to_tensor(list(it.zip_longest(*inputs, fillvalue=0)), dtype=tf.int32)
-    targets = tf.convert_to_tensor(list(it.zip_longest(*inputs, fillvalue=0)), dtype=tf.int32)
-    sequence_lengths = tf.convert_to_tensor([len(i) for i in inputs], dtype=tf.int32)
-
-    return tf_inputs, targets, sequence_lengths
-
-
-def get_test_input(config):
-    test_input = tf.convert_to_tensor([[i] for i in range(0, config.num_classes, config.test_step_size)], dtype=tf.int32)
-    test_targets = tf.convert_to_tensor([[i] for i in range(0, config.num_classes, config.test_step_size)], dtype=tf.int32)
-
-    return test_input, test_targets
-
-
-def sequence_cross_entropy(labels, logits, sequence_lengths):
-    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
-    if sequence_lengths is not None:
-        loss_sum = tf.reduce_sum(cross_entropy, axis=0)
-        return tf.truediv(loss_sum, tf.cast(sequence_lengths, tf.float32))
-    else:
-        return tf.reduce_mean(cross_entropy, axis=0)
-
-
-class Model:
-
-    def __init__(self, config,  inputs, targets, batch_size, sequence_lengths=None):
-        global_step = tf.Variable(0, trainable=False)
-
-        # cell = tf.contrib.rnn.BasicLSTMCell(num_units=size, forget_bias=0.0, state_is_tuple=True)
-        # cell = tf.contrib.rnn.LSTMBlockCell(num_units=size, forget_bias=1.0)
-        #
-        # with tf.name_scope("initial_state"):
-        #     initial_state = cell.zero_state(batch_size=batch_size, dtype=tf.float32)
-        #     # tf.summary.tensor_summary('initial_state', initial_state)
-
-        embedding = tf.get_variable("embedding", [config.num_classes, config.num_units], dtype=tf.float32)
-        _inputs = tf.nn.embedding_lookup(embedding, inputs)
-
-        # outputs, state = tf.contrib.rnn.static_rnn(cell, _inputs, initial_state=initial_state, scope="rnn", sequence_length=sequence_lengths)
-
-        lstm = tf.contrib.rnn.LSTMBlockFusedCell(num_units=config.num_units, forget_bias=1.0, cell_clip=None, use_peephole=False)
-
-        initial_state = (tf.zeros([batch_size, config.num_units], tf.float32), tf.zeros([batch_size, config.num_units], tf.float32))
-
-        output, state = lstm(_inputs, initial_state=initial_state, dtype=None, sequence_length=sequence_lengths, scope="rnn")
-
-
-
-        softmax_w = tf.get_variable("softmax_w", [config.num_units, config.num_classes], dtype=tf.float32)
-        softmax_b = tf.get_variable("softmax_b", [config.num_classes], dtype=tf.float32)
-        # logits = [tf.matmul(output, softmax_w) + softmax_b for output in outputs]
-
-        _output = tf.reshape(output, [-1, config.num_units])
-        _logits = tf.matmul(_output, softmax_w) + softmax_b
-
-        logits = tf.reshape(_logits, [-1, batch_size, config.num_classes])
-
-        loss = tf.reduce_mean(sequence_cross_entropy(labels=targets, logits=logits, sequence_lengths=sequence_lengths))
-
-        starting_learning_rate = 0.1
-        learning_rate = tf.train.exponential_decay(starting_learning_rate, global_step, 10, 0.96, staircase=True)
-
-        optimizer = tf.train.AdamOptimizer(learning_rate)
-
-        trainable_vars = tf.trainable_variables()
-
-        train_step = optimizer.minimize(loss, var_list=trainable_vars, global_step=global_step)
-
-        self.learning_rate = learning_rate
-        self.logits = logits
-        self.loss = loss
-        self.train_step = train_step
-
-
-def train(config):
-    with tf.Session() as session:
-
-        summary_writer = tf.summary.FileWriter("logs/")
-
-        with tf.name_scope("Train"):
-            inputs, targets, sequence_lengths = get_input(config)
-
-            with tf.variable_scope("Model", reuse=None):
-                train_model = Model(config, inputs, targets, len(config.num_seq), sequence_lengths=sequence_lengths)
-
-            tf.summary.scalar("loss", train_model.loss)
-            tf.summary.scalar("learning rate", train_model.learning_rate)
-
-
-
-        merged = tf.summary.merge_all()
-        init_op = tf.global_variables_initializer()
-
-        summary_writer.add_graph(session.graph)
-
-        session.run(init_op)
-
-        for i in range(config.epochs):
-            if i % 10 == 0:
-                print(i)
-                summary = session.run(merged)
-                summary_writer.add_summary(summary, i)
-
-            session.run(train_model.train_step)
-
-
-
-def test(config):
-    with tf.Session() as session:
-
-        summary_writer = tf.summary.FileWriter("logs/")
-
-        with tf.name_scope("Test"):
-            test_inputs, test_targets = get_test_input(config)
-
-            with tf.variable_scope("Model", reuse=True):
-                test_model = Model(config, test_inputs, test_targets, 1)
-
-            tf.summary.scalar("loss", test_model.loss)
-
-        merged = tf.summary.merge_all()
-        init_op = tf.global_variables_initializer()
-
-        session.run(init_op)
-
-
-        logits = test_model.logits
-        loss = test_model.loss
-
-        summary, out, targ, loss = session.run([merged, logits, test_targets, loss])
-
-        # np.set_printoptions(precision=5)
-        pretty_out= np.array([[np.argmax(batch) for batch in batches] for batches in out])
-        pretty_targets = np.array([i.tolist() for i in targ])
-
-        print(loss)
-        print(np.swapaxes(np.array([pretty_out, pretty_targets]), 0, 2))
+    train_dataset, test_dataset = get_dummy_datasets(num_classes)
 
 
 if __name__ == '__main__':
-    config = ModelConfig()
-    train(config)
+    config = DummyModelConfig()
+
+    tf.reset_default_graph()
+    summary_writer = tf.summary.FileWriter("logs/")
+
+    lstm.train(config, summary_writer)
+    lstm.test(config, summary_writer)
+
+    summary_writer.add_graph(tf.get_default_graph())
+    summary_writer.close()
 
     # dataset = reader.dataset160()
     # print(dataset.partition(10)[0])
